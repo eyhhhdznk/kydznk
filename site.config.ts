68a0b001a670a21f675b7ccfc99d17052ecf9634
忾氧 <99227507+eyhhhdznk@users.noreply.github.com>
2023-02-03 20:08:38 +0800

    Update site.config.ts

--- site.config.ts
+++ site.config.ts
@@ -24,7 +24,7 @@ export default siteConfig({
 
 
   // default notion icon and cover images for site-wide consistency (optional)
-  // page-specific values will override these site-wide defaults
+  page-specific values will override these site-wide defaults
   defaultPageIcon: null,
   defaultPageCover: null,
   defaultPageCoverPosition: 0.5,
